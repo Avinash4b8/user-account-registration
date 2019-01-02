12fecdb7dea063243ab8afc95350589d79152a67
DESKTOP-G82MGVG\avina <Avinash4b8@gmail.com>
2019-01-02 15:01:18 -0500

    added ssl certificate

--- src/main/java/com/avinash/registration/configuration/SecurityConfiguration.java
+++ src/main/java/com/avinash/registration/configuration/SecurityConfiguration.java
@@ -1,5 +1,12 @@
 package com.avinash.registration.configuration;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,9 +18,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/register").permitAll()
-		.antMatchers("/confirm").permitAll();
+		http.authorizeRequests().antMatchers("/register").permitAll().antMatchers("/confirm").permitAll();
 
 	}
 
--- src/main/java/com/avinash/registration/repository/UserRepository.java
+++ src/main/java/com/avinash/registration/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.avinash.registration.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.avinash.registration.entity.User;
 
+@Repository("userRepository")
 public interface UserRepository extends CrudRepository<User, Long> {
 
 	User findByEmail(String email);
